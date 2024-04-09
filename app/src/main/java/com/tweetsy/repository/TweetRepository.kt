c0f8a34e672f61c645cb272ad0750332314d66b2
Sash1391 <sash@Shashikirans-MacBook-Pro.local>
2024-04-09 16:15:23 +0530

    Tweesty second draft - Creation of viewmodels, repostory, jetpack compose screen and components, hilt-navigtion

---
+++ app/src/main/java/com/tweetsy/repository/TweetRepository.kt
@@ -0,0 +1,38 @@
+package com.tweetsy.repository
+
+import com.tweetsy.api.TweetsyAPI
+import com.tweetsy.model.TweetListItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+/*
+   TweetRepository: A repository class helps to fetch date from API class and pass to
+   view model.
+ */
+class TweetRepository @Inject constructor(private val tweetsyAPI: TweetsyAPI) {
+
+    private val _categories = MutableStateFlow<List<String>>(emptyList())
+    val categories: StateFlow<List<String>>
+        get() = _categories
+
+    private val _tweets = MutableStateFlow<List<TweetListItem>>(emptyList())
+    val tweets: StateFlow<List<TweetListItem>>
+        get() = _tweets
+
+    suspend fun getCategories() {
+        val response = tweetsyAPI.getCategory()
+        if (response.isSuccessful && response.body() != null) {
+            // emit data.
+            _categories.emit(response.body()!!)
+        }
+    }
+
+    suspend fun getTweets(category: String){
+        val response = tweetsyAPI.getTweets("tweets[?(@.category==\"${category}\")]")
+        if(response.isSuccessful && response.body() != null) {
+            _tweets.emit(response.body()!!)
+        }
+    }
+}
+
